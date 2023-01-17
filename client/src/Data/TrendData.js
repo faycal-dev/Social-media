7107d7e8791fe2ea367b7957bca915e2ab204a4b
faycal-dev <m.elmogherbi@esi-sba.dz>
2023-01-17 17:46:27 +0100

    real time chat

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+  {
+    name: "BarcaVSMadrid",
+    shares: 250,
+  },
+  {
+    name: "Chan2023",
+    shares: 90,
+  },
+  {
+    name: "ChatGPT",
+    shares: 75.5,
+  },
+  {
+    name: "Reactjs",
+    shares: 63,
+  },
+  {
+    name: "Elon Musk",
+    shares: 55,
+  },
+];
